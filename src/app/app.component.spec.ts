309f997cc83c639c466a24b78eac52446a611ee8
Lutyvr02 <lutyvr02@gmail.com>
2024-09-19 21:30:21 -0400

    creted and eddited some things

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-course-2024');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Buenas Tardes');
   });
 });
