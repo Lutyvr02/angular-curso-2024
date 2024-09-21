309ab7fef5039b683760cb5b9b99e8c2b6f6b1b1
Lutyvr02 <lutyvr02@gmail.com>
2024-09-20 20:45:47 -0400

    added the FOR with even numbers date: 9/20/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-course-2024';  
-
   constructor(){
     console.log('subtract', this.sub(8,4))
   }
@@ -22,4 +21,14 @@ export class AppComponent {
   private sub (num1:number, num2:number): number {
     return num1 - num2
   }
+
+  public getArray(){
+    const persons:number[] = [1,2,3,4,5]
+    for(let i = 0; i < persons.length; i++){
+      if(persons[i]  % 2 === 0){
+        console.log('person =', persons[i])
+      }
+    }
+  }
+
 }
