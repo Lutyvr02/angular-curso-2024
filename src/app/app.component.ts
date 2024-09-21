21eef2c510e321def0dbb63c450eb7c0e0026b19
Lutyvr02 <lutyvr02@gmail.com>
2024-09-20 20:23:06 -0400

    added private substraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-course-2024';
+  title = 'angular-course-2024';  
 
-  num1 = 12;
-  num2 = 12;
-  
+  constructor(){
+    console.log('subtract', this.sub(8,4))
+  }
 
+  public sum (num1:number, num2:number): number {
+    return num1 + num2
+  }
+
+  private sub (num1:number, num2:number): number {
+    return num1 - num2
+  }
 }
