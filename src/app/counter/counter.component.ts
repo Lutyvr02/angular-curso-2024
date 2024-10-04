eea4fdff57bea7baece54bd944506a2f26b86582
Lutyvr02 <lutyvr02@gmail.com>
2024-10-03 20:33:19 -0400

    added faces after render, after next render, date: 3/10/2024

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -9,10 +9,29 @@ import { Component, afterRender, afterNextRender } from '@angular/core';
 })
 export class CounterComponent {
   sum: number = 0;
+  appBackGround:string = 'red'
 
   constructor() {
-    afterRender(() => {
-      console.log('after render: '+ this.sum)
+    afterRender({
+      earlyRead: () => {
+        const currentAppColor = this.appBackGround
+        return 'Form earlyRead: ' + currentAppColor
+      },
+      mixedReadWrite:(props) => {
+        if(props.indexOf('red') > -1){
+          this.appBackGround = 'green'
+        } else {
+          this.appBackGround = 'red'
+        }
+        return 'From mixedReadWrite: '+ this.appBackGround
+      },
+      write:(props) => {
+        document.body.style.backgroundColor = this.appBackGround
+        return 'FROM write: '+ this.appBackGround
+      },
+      read:(props) => {
+        const newBackGround = this.appBackGround
+      },
     });
 
     afterNextRender(() => {
