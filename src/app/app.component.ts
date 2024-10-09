f3002e5dff9de6f6a9a7aec1078eedbae7790eb6
Lutyvr02 <lutyvr02@gmail.com>
2024-10-08 21:01:08 -0400

    added the subscribe from rxjs with filter, from, map and tap date: 8/101/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,70 +5,86 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { CommonModule } from '@angular/common';
 import { PersonListComponent } from './person-list/person-list.component';
 import { CounterComponent } from './counter/counter.component';
+import { filter, from, map, tap } from 'rxjs';
 
-interface IPerson{
-  name:string,
-  lastname:string,
-  age:number
+interface IPerson {
+  name: string;
+  lastname: string;
+  age: number;
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent, CalculatorComponent, CommonModule, PersonListComponent, CounterComponent], 
+  imports: [
+    RouterOutlet,
+    UserCardComponent,
+    CalculatorComponent,
+    CommonModule,
+    PersonListComponent,
+    CounterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  users = [
+    { name: 'abc', email: 'abc@gmail.com' },
+    { name: 'tyty', email: 'tyty@gmail.com' },
+  ];
+  selectedUser: any = this.users[0];
 
-  users = [{name: 'abc', email: 'abc@gmail.com'}, {name: 'tyty', email: 'tyty@gmail.com'}]
-  selectedUser:any = this.users[0];
-
-  result = 0
-  title = 'angular-course-2024';  
-  animals:string[] = ['a','b','c','d','e','f','g']
+  result = 0;
+  title = 'angular-course-2024';
+  animals: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
   person: IPerson = {
     name: 'juan',
     lastname: 'perez',
-    age: 12
-  }
+    age: 12,
+  };
 
-  students:number[] = [1,2,3,4,5,6]
-  parents:number[] = [7,8,9,10]
+  students: number[] = [1, 2, 3, 4, 5, 6];
+  parents: number[] = [7, 8, 9, 10];
 
-  var1 = 0
-  var2 = null
-  var3 = 'hola'
+  var1 = 0;
+  var2 = null;
+  var3 = 'hola';
 
-  constructor(){
- /*   const {name, age} = this.person
+  youtube = from([1, 2, 3, 4, 5, 6]);
+
+  constructor() {
+    /*   const {name, age} = this.person
     console.log('desustruraccion', name, age)
 
     let both = [...this.students,...this.parents]
   /*  console.log("spread proyector: "+both)*/
 
-    console.log('REST operator: ', this.sum2(2,4,6))
+    console.log('REST operator: ', this.sum2(2, 4, 6));
 
-    console.log('Nullish Coalesing: ', this.var2 ??  this.var3)
-    
-  /*  console.log('OR: ', this.var1 ||  this.var2)*/
+    console.log('Nullish Coalesing: ', this.var2 ?? this.var3);
+
+    /*  console.log('OR: ', this.var1 ||  this.var2)*/
 
     /*console.log('MAP: ', this.animals.map( (animal) => {animal + 'new'}))
     console.log('FOREACH: ', this.animals.forEach((animal)=>{animal + 'new'}))
     console.log('FIND', this.animals.find((animal)=> animal == 'b'))
     console.log('FILTER', this.animals.filter((animal)=> animal == 'c'))
     console.log('INDEXOF', this.animals.indexOf('c'))*/
+
+    this.youtube.subscribe((res) => {
+      console.log('sub 1: ', res);
+    });
   }
 
-  public sum (num1:number, num2:number): number {
-    return num1 + num2
+  public sum(num1: number, num2: number): number {
+    return num1 + num2;
   }
 
-  private sub (num1:number, num2:number): number {
-    return num1 - num2
+  private sub(num1: number, num2: number): number {
+    return num1 - num2;
   }
-/*
+  /*
   public getArray(){
     const persons:number[] = [1,2,3,4,5]
     for(let i = 0; i < persons.length; i++){
@@ -78,18 +94,41 @@ export class AppComponent {
     }
   }*/
 
-  public sum2(...persons:number[]){
+  public sum2(...persons: number[]) {
     //return persons[0] + persons[1]
-    return persons.reduce((acumulador, valorActual) => (acumulador+ valorActual))
+    return persons.reduce(
+      (acumulador, valorActual) => acumulador + valorActual
+    );
   }
 
-  public recieveData(data:any){
-    console.log('Print: '+data) 
+  public recieveData(data: any) {
+    console.log('Print: ' + data);
   }
 
-  public onResult(event:any){
-    this.result = event ?? 0
+  public onResult(event: any) {
+    this.result = event ?? 0;
   }
 
-  userCardCreated: boolean = true
+  addVideo() {
+    this.youtube
+      .pipe(
+        map((res) => {
+          console.log('map operator rxjs: ', res);
+          if (res % 2 == 0) {
+            return res;
+          } else {
+            return null;
+          }
+        }),
+        tap((res) => {
+          console.log('VAlue: ', res);
+        }),
+        filter((res: number | null) => res != null)
+      )
+      .subscribe((res) => {
+        console.log('sub 2: ', res);
+      });
+  }
+
+  userCardCreated: boolean = true;
 }
