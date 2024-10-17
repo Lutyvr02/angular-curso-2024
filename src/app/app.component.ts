62885048549875a031bab57e4eeb87c31e68cca0
Lutyvr02 <lutyvr02@gmail.com>
2024-10-16 20:46:16 -0400

    added boostrap with some new features win routes date: 16/10/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { UserCardComponent } from './user-card/user-card.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CommonModule } from '@angular/common';
@@ -34,7 +34,8 @@ interface IPerson {
     PurePipe,
     ImpurePipe,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterLink
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UserCardComponent } from './user-card/user-card.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'user-card',
+        component: UserCardComponent
+    },
+    {
+        path: 'cal',
+        component: CalculatorComponent
+    },
+    {
+        path: 'counter-nav',
+        loadComponent: () => import('./counter/counter.component').then (m => m.CounterComponent)
+    }
+];
